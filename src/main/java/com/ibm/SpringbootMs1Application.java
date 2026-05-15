3007db7da216d6c745853de639bccc21cb256d1e
Mohamed Ashif <mohamed.ashif1@ibm.com>
2026-05-15 15:09:46 +0530

    changed MAIN class to register API'S

--- src/main/java/com/ibm/SpringbootMs1Application.java
+++ src/main/java/com/ibm/SpringbootMs1Application.java
@@ -2,11 +2,19 @@ package com.ibm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringbootMs1Application {
+public class SpringbootMs1Application extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootMs1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMs1Application.class, args);
