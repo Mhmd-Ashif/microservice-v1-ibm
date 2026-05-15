ab340cd5dec9a735c44efb0e6979e1f4912ccc22
Ashif <114198892+Mhmd-Ashif@users.noreply.github.com>
2026-05-15 11:58:31 +0530

    Update console output message in main application

--- src/main/java/com/ibm/SpringbootMs1Application.java
+++ src/main/java/com/ibm/SpringbootMs1Application.java
@@ -10,7 +10,7 @@ public class SpringbootMs1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMs1Application.class, args);
-		System.out.println("it is running in via jenkins - v2");
+		System.out.println("it is running in via jenkins - new dep ");
 	}
 
 }
